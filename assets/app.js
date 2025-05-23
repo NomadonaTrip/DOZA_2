ffc2a56f3b5a71e1e843d72f28e94986be17cd13
NomadonaTrip <telltaio@gmail.com>
2025-05-23 01:43:47 -0400

    Shippable version of home page

--- assets/app.js
+++ assets/app.js
@@ -14,8 +14,8 @@ class SpecialHeader extends HTMLElement {
                 <ul class="navigation">
                     <a href="index.html"><li>Home</li></a>
                     <a href="about-us.html"><li>About Us</li></a>
-                    <a href="contact-us.html"><li>Our Ministries</li></a>
-                    <a href="contact-us.html"><li>Give</li></a>
+                    <a href="our-ministries.html"><li>Our Ministries</li></a>
+                    <a href="give.html"><li>Give</li></a>
                     <a id="call-button" href=""><li><button>Contact Us</button></li></a>
                 </ul>
                 <label class="hamburger-menu">
@@ -46,4 +46,45 @@ class SpecialHeader extends HTMLElement {
   }
 }
 
-customElements.define('special-header', SpecialHeader);
+customElements.define("special-header", SpecialHeader);
+// The special footer starts here
+
+class SpecialFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <!-- Footer section -->
+    <footer class="footer">
+      <div class="footer-content">
+        <div class="footer-section">
+          <h3>Contact Us</h3>
+          <p>123 Revival Street<br>
+          Mississauga, ON 12345</p>
+          <p>Phone: (555) 123-4567<br>
+          Email: info@revivalcentercanada.ca</p>
+        </div>
+        <div class="footer-section">
+          <h3>Service Times</h3>
+          <p>Sunday Service: 10:00 AM<br>
+          Wednesday Prayer: 7:00 PM<br>
+          Monthly Revival: First Friday 6:00 PM</p>
+        </div>
+        <div class="footer-section">
+          <h3>Connect With Us</h3>
+          <p>
+            <a href="#">Facebook</a><br>
+            <a href="#">YouTube</a><br>
+            <a href="#">Instagram</a><br>
+            <a href="#">Twitter</a>
+          </p>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <p>&copy; 2025 The Revival Center. All rights reserved.</p>        
+        <p class="credits">Built by Orban Forest Inc.</p>
+      </div>
+    </footer>
+    `;
+  }
+}
+
+customElements.define("special-footer", SpecialFooter);
